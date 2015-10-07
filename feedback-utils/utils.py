66119cb7b7de11d23bf7e8259841886f31fb06e0
loekj <ljanssen@stanford.edu>
2015-10-06 22:58:23 -0700

    mend

---
+++ feedback-utils/utils.py
@@ -0,0 +1,8 @@
+def mean(l):
+	return float(sum(l))/len(l) if len(l) > 0 else float('nan') 
+
+def sampleStd(l):
+	if len(l) == 0:
+		return float('nan')
+	avg = mean(l)
+	return float( sum( map(lambda x: (x - avg)**2, l) ) ) / (len(l) - 1)
